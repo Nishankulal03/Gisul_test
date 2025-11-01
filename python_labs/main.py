0b17374156c2e973c8586b6eb2ac0a9d06f8daf8
Nishankulal03 <nishankulal03@example.com>
2025-11-01 11:19:20 +0530

    Add python labs folder

---
+++ python_labs/main.py
@@ -0,0 +1,5 @@
+# Challenge 1: Print "Hello, <name>!"
+# TODO: Get user input and print greeting
+
+# Challenge 2: Sum of numbers using a loop
+# TODO: Get a number and print sum of numbers from 1 to n
